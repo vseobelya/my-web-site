72e40ca25632308ae0e0174d3b15caa0d2f77751
vseobelya <vseobelya@gmail.com>
2021-12-04 13:31:08 +0300

    Changed app.component files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {Good} from './shared/tovar';
+import {test} from './shared/data';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  test: Good[] = test;
   title = 'my-web-site';
 }
